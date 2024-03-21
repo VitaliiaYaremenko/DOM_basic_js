49d9fb229f1c9324911aa3a4db0b16de08222274
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-03-21 14:16:03 +0100

    added changes form

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,41 +1,57 @@
+const DATA_KEY = 'formData';
+
 (function () {
-  const form = document.querySelector('#form');
-  const dataFormKey = 'formData';
+  const formPage = () => {
+    const form = document.querySelector('[data-form]');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
 
-    const formElements = event.target.querySelectorAll('input, select, textarea');
-    const formData = Array.from(formElements).reduce((accum, item) => {
-      accum[item.name] = item.value;
-      return accum;
-    }, {});
+      const formInputs = event.target.querySelectorAll('input, select, textarea');
+      const InputsData = Array.from(formInputs).reduce((accum, item) => {
+        accum[item.name] = item.value;
+        return accum;
+      }, {});
 
-    for (const key in formData) {
-      if (formData[key] === '') {
-        const createMassage = document.createElement('div');
-        createMassage.className = 'userMessage standard mb-4 mt-2 text-center';
-        createMassage.innerHTML = '<strong>Не отримано дані!</strong> Заповніть форму!';
-        form.prepend(createMassage);
-        return;
+      for (const key in InputsData) {
+        if (InputsData[key] === '') {
+          const createMassage = document.createElement('div');
+          createMassage.className = 'userMessage standard mb-4 mt-2 text-center';
+          createMassage.innerHTML = '<strong>Не отримано дані!</strong> Заповніть форму!';
+          form.prepend(createMassage);
+          return;
+        }
       }
+      localStorage.setItem(DATA_KEY, JSON.stringify(InputsData));
+      event.target.reset();
+      const url = `${window.location.origin}/DOM_basic_js/src/about.html`;
+      window.open(url, '_blank');
+    });
+  };
+
+  const outPutPage = () => {
+    const data = JSON.parse(localStorage.getItem(DATA_KEY));
+    const dataContent = document.querySelector('[data-content]');
+
+    if (!data) {
+      dataContent.innerHTML = 'Дані не передані!';
+      return;
     }
-    localStorage.setItem(dataFormKey, JSON.stringify(formData));
-    window.location.href = 'about.html';
 
-    // ______
-    const data = JSON.parse(localStorage.getItem(dataFormKey));
+    const ul = document.createElement('ul');
+    dataContent.append(ul);
 
-    if (data && window.location.pathname === '/about.html') {
-      const ulItem = document.querySelector('#dataUserItem');
-
-      for (const key in data) {
-        const createNewLi = document.createElement('li');
-        createNewLi.className = 'data-user-list';
-        createNewLi.innerHTML = `${key}: ${data}`;
-        ulItem.appendChild(createNewLi);
-        return;
-      }
+    for (const key in data) {
+      const li = document.createElement('li');
+      li.className = 'list-group-item';
+      li.innerHTML = `<b>${key}</b>: ${data[key]}`;
+      ul.append(li);
     }
-  });
+  };
+
+  if (location.pathname.includes('index.html')) {
+    formPage();
+  } else if (location.pathname.includes('about.html')) {
+    outPutPage();
+  }
 }());
